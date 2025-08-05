8dafa4ade9e9aaee4af7c52b6e05959931bba954
Salai Wai Yan Kyaw <waiyankyaw711998@gmail.com>
2025-08-05 14:59:27 +0630

    viewproduct created

--- admin/insertProduct2.php
+++ admin/insertProduct2.php
@@ -1,27 +1,21 @@
 <?php
-require_once"dbconnect.php";
-if(!isset($_SESSION))
-{//session will be opened when it does not exist
+require_once "dbconnect.php";
+if (!isset($_SESSION)) { //session will be opened when it does not exist
     session_start();
 }
-try
-    {
-        $sql = "select * from category";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        //$stmt->fetch();
-        $categories = $stmt->fetchAll();
-
-    }
-    catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    }
+try {
+    $sql = "select * from category";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    //$stmt->fetch();
+    $categories = $stmt->fetchAll();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
 
 
-if(isset($_POST['insertBtn']))
-{
+if (isset($_POST['insertBtn'])) {
     $name = $_POST['pname'];
     $price = $_POST['price'];
     $category = $_POST['category'];
@@ -36,47 +30,39 @@ if(isset($_POST['insertBtn']))
     // echo $fileImage['name']."<br>";
     // echo "fileImage[size]<br>";
     // echo "fileImage[type]<br>";
-    $filePath = "productImage/" .$fileImage['name'];
+    $filePath = "productImage/" . $fileImage['name'];
     //uploading to a specified directory
 
-    $status = move_uploaded_file($fileImage['tmp_name'], $filePath);//image, destination
+    $status = move_uploaded_file($fileImage['tmp_name'], $filePath); //image, destination
 
-    if ($status)
-    {
-      try//inserting data into database
-      {
-        //productID	productName	category	price	description	qty	imgPath	
-        $sql = "insert into products values (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $flag = $stmt->execute([null,$name, $category, $price, $description, $qty, $filePath]);
-        $id = $conn->lastInsertId();
-        
-        if($flag)
+    if ($status) {
+        try //inserting data into database
         {
-            $message = "New product with id $id has been inserted successfully!";
-            $_SESSION['message'] = $message;
-            header("Location: viewProduct.php");
+            //productID	productName	category	price	description	qty	imgPath	
+            $sql = "insert into products values (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $flag = $stmt->execute([null, $name, $category, $price, $description, $qty, $filePath]);
+            $id = $conn->lastInsertId();
+
+            if ($flag) {
+                $message = "New product with id $id has been inserted successfully!";
+                $_SESSION['message'] = $message;
+                header("Location: viewProduct.php");
+            } else {
+                echo "Fail";
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
-        else
-        {
-            echo "Fail";
-        }
-      }
-      catch(PDOException $e)
-      {
-        echo $e->getMessage();
-      }
-    }
-    else
-    {
+    } else {
         echo "file upload fail";
     }
-
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -85,63 +71,63 @@ if(isset($_POST['insertBtn']))
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row mt-3 mx-auto">
-            <?php require_once "navbarcopy.php";?>
+            <?php require_once "navbarcopy.php"; ?>
         </div>
         <div class="row">
             <div class="col-md-12 mt-3 px-5">
                 <h4 class="text-center">Insert Product</h4>
                 <form action="insertProduct2.php" method="post" enctype="multipart/form-data">
                     <div class="row">
-                    <div class="col-md-5 mx-3">
-                        <div class="mb-1">
-                            <label for="pname" class="form-label">Product Name</label>
-                            <input type="text" class="form-control" name="pname">
-                        </div>
-                         <div class="mb-1">
-                            <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" name="price">
-                        </div>
-                        <select name="category" class="form-select">
-                        <option value="">Choose Category</option>
-                            <?php
-                            if(isset($categories))
-                            {
-                                foreach($categories as $category)
-                                    {
+                        <div class="col-md-5 mx-3">
+                            <div class="mb-1">
+                                <label for="pname" class="form-label">Product Name</label>
+                                <input type="text" class="form-control" name="pname">
+                            </div>
+                            <div class="mb-1">
+                                <label for="price" class="form-label">Price</label>
+                                <input type="number" class="form-control" name="price">
+                            </div>
+                            <select name="category" class="form-select">
+                                <option value="">Choose Category</option>
+                                <?php
+                                if (isset($categories)) {
+                                    foreach ($categories as $category) {
                                         echo "<option value=$category[catID]> $category[catName] </option>";
-                                    }   
-                            }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="col-md-5 mx-3 px-5">
-                        <div class="mb-1">
-                            <label class="form-label">Quantity</label>
-                            <input type="number" class="form-control" name="qty">
+                                    }
+                                }
+                                ?>
+                            </select>
                         </div>
-                        <div class="mb-1">
-                            <textarea name="description" id="" class="form-control">
+                        <div class="col-md-5 mx-3 px-5">
+                            <div class="mb-1">
+                                <label class="form-label">Quantity</label>
+                                <input type="number" class="form-control" name="qty">
+                            </div>
+                            <div class="mb-1">
+                                <textarea name="description" id="" class="form-control">
 
                             </textarea>
+                            </div>
+                            <div class="mb-1">
+                                <label for="" class="form-label">Choose Product Image</label>
+                                <input type="file" class="form-control" name="productImage">
+                            </div>
+                            <button type="submit" name="insertBtn" class="btn btn-primary rounded-pill mt-3">Insert Product</button>
                         </div>
-                        <div class="mb-1">
-                            <label for="" class="form-label">Choose Product Image</label>
-                            <input type="file" class="form-control" name="productImage">
-                        </div>
-                        <button type="submit" name="insertBtn" class="btn btn-primary rounded-pill mt-3">Insert Product</button>
-                    </div>
 
                 </form>
             </div>
-            </div>
-
         </div>
+
+    </div>
 
 
     </div>
-    
+
 </body>
+
 </html>
