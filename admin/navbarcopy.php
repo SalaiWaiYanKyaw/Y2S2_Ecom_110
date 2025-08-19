953440914cdd86122564ea33d5844f50346bb3d0
Salai Wai Yan Kyaw <waiyankyaw711998@gmail.com>
2025-08-19 16:20:10 +0630

    admin login and logout updated

--- admin/navbarcopy.php
+++ admin/navbarcopy.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary border border-dark rounded p-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="image/logo.jfif" style = "width: 80px; height: 80px;" class="img-responsive"> </a>
+    <a class="navbar-brand" href="#"><img src="image/logo.jfif" style="width: 80px; height: 80px;" class="img-responsive"> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,18 +19,29 @@
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
+        <?php
+          if(isset($_SESSION['loginSuccess'])) { ?>
         <li class="nav-item">
           <a class="nav-link" href="viewProduct.php">View Product</a>
         </li>
+        <li class = "nav-item">
+          <?php echo "<p class='nav-link disabled'>$_SESSION[email]</p>";?>
+        </li>
+        <li class="nav-item">
+          <a href="logout.php" class="nav-link">Logout</a>
+        </li>
       </ul>
       <form class="d-flex" role="search" method="get" action="viewProduct.php">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="tsearch"/>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="tsearch" />
         <button name="bsearch" class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <?php    }?>
     </div>
   </div>
 </nav>
